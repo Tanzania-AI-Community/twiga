8cbd84cb2e542600d1d0c8b8c881f8e20720892e
Victor Oldensand <victormag24@gmail.com>
2024-11-27 18:12:11 +0100

    Misc: improve conciseness by getting rid of background tasks

--- app/utils/background_tasks_utils.py
+++
@@ -1,7 +0,0 @@
-from fastapi import BackgroundTasks, Request
-
-
-def add_background_task(
-    background_tasks: BackgroundTasks, task_func, request: Request, *args, **kwargs
-):
-    background_tasks.add_task(task_func, request, *args, **kwargs)
