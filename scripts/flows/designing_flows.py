6dc7023ba322e3511b096b6e3924b0bd4684d7ec
Timothy Besada <tibes.91@gmail.com>
2024-12-21 16:59:54 +0100

    chore(scripts): fixes type errors for scripts

--- scripts/flows/designing_flows.py
+++ scripts/flows/designing_flows.py
@@ -9,53 +9,57 @@ import app.utils.flow_utils as futil
 logger = logging.getLogger(__name__)
 
 
-async def handle_select_classes_init_action(user: User) -> Dict[str, Any]:
-    try:
-        subject_id = 1  # Hardcoded subject_id as 1, because init action is only used when testing
-        subject_data = await db.get_subject_grade_levels(subject_id)
-        subject_title = subject_data["subject_name"]
-        classes = subject_data["classes"]
-        logger.debug(f"Subject title for subject ID {subject_id}: {subject_title}")
-        logger.debug(f"Available classes for subject ID {subject_id}: {classes}")
+# async def handle_select_classes_init_action(
+#     user: User,
+# ) -> Dict[str, Any] | PlainTextResponse:
+#     try:
+#         subject_id = 1  # Hardcoded subject_id as 1, because init action is only used when testing
+#         subject_data = await db.get_subject_grade_levels(subject_id)
+#         subject_title = subject_data["subject_name"]
+#         classes = subject_data["classes"]
+#         logger.debug(f"Subject title for subject ID {subject_id}: {subject_title}")
+#         logger.debug(f"Available classes for subject ID {subject_id}: {classes}")
+#
+#         select_class_question_text = f"Select the class you are in for {subject_title}."
+#         select_class_text = f"This helps us find the best answers for your questions in {subject_title}."
+#         no_classes_text = (
+#             f"Sorry, currently there are no active classes for {subject_title}."
+#         )
+#         has_classes = len(classes) > 0
+#
+#         response_payload = futil.create_flow_response_payload(
+#             screen="select_classes",
+#             data={
+#                 "classes": (
+#                     classes
+#                     if has_classes
+#                     else [
+#                         {
+#                             "id": "0",
+#                             "title": "No classes available",
+#                         }
+#                     ]
+#                 ),
+#                 "has_classes": has_classes,
+#                 "no_classes_text": no_classes_text,
+#                 "select_class_text": select_class_text,
+#                 "select_class_question_text": select_class_question_text,
+#                 "subject_id": str(subject_id),
+#             },
+#         )
+#
+#         return response_payload
+#
+#     except ValueError as e:
+#         return PlainTextResponse(
+#             content={"error_msg": str(e)},
+#             status_code=422,
+#         )
 
-        select_class_question_text = f"Select the class you are in for {subject_title}."
-        select_class_text = f"This helps us find the best answers for your questions in {subject_title}."
-        no_classes_text = (
-            f"Sorry, currently there are no active classes for {subject_title}."
-        )
-        has_classes = len(classes) > 0
 
-        response_payload = futil.create_flow_response_payload(
-            screen="select_classes",
-            data={
-                "classes": (
-                    classes
-                    if has_classes
-                    else [
-                        {
-                            "id": "0",
-                            "title": "No classes available",
-                        }
-                    ]
-                ),
-                "has_classes": has_classes,
-                "no_classes_text": no_classes_text,
-                "select_class_text": select_class_text,
-                "select_class_question_text": select_class_question_text,
-                "subject_id": str(subject_id),
-            },
-        )
-
-        return response_payload
-
-    except ValueError as e:
-        return PlainTextResponse(
-            content={"error_msg": str(e)},
-            status_code=422,
-        )
-
-
-async def handle_onboarding_init_action(user: User) -> Dict[str, Any]:
+async def handle_onboarding_init_action(
+    user: User,
+) -> Dict[str, Any] | PlainTextResponse:
     try:
         response_payload = futil.create_flow_response_payload(
             screen="personal_info",
@@ -68,7 +72,9 @@ async def handle_onboarding_init_action(user: User) -> Dict[str, Any]:
         return PlainTextResponse(content={"error_msg": str(e)}, status_code=422)
 
 
-async def handle_select_subjects_init_action(user: User) -> Dict[str, Any]:
+async def handle_select_subjects_init_action(
+    user: User,
+) -> Dict[str, Any] | PlainTextResponse:
     try:
         # Get available subjects from the database
         subjects = await db.get_available_subjects()
