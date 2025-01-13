bf9f2b5695ccaf289980023e6814bbd3f985d6bc
Victor Oldensand <victormag24@gmail.com>
2025-01-13 21:55:29 +0100

    misc: fix formatting/linting issues

--- app/main.py
+++ app/main.py
@@ -9,7 +9,6 @@ from app.services.whatsapp_service import whatsapp_client
 from app.services.request_service import handle_request
 from app.database.engine import db_engine, init_db
 from app.services.flow_service import flow_client
-from fastapi import Request
 
 
 logger = logging.getLogger(__name__)
