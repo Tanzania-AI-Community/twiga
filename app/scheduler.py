2b4020103403a8ff8f52e93cd987f4414b6bff94
Fredy German <fredgerman30@gmail.com>
2025-01-25 16:51:23 +0300

    feat :  log limits data before reseting

--- app/scheduler.py
+++ app/scheduler.py
@@ -12,6 +12,12 @@ EAT = pytz.timezone(settings.timezone)
 
 async def reset_daily_limits():
     redis_client = get_redis_client()
+    # Note: Maybe before deleting the keys, we can send a report of the daily usage to the admin
+    # Log all the keys and their values before deleting them
+    keys = await redis_client.keys("rate_limit:*")
+    for key in keys:
+        value = await redis_client.get(key)
+        logger.info(f"Key: {key}, Value: {value}")
     keys = await redis_client.keys("rate_limit:*")
     for key in keys:
         await redis_client.delete(key)
